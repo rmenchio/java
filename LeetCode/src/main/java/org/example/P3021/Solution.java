34e4f1ab869fc4998c4d9c539ec4d7f8f068fd50
Rodrigo Menchio <rodrigomenchio@id.uff.br>
2025-08-29 07:39:49 -0300

    [MEDIUM] solving 3021. Alice and Bob Playing Flower Game

---
+++ LeetCode/src/main/java/org/example/P3021/Solution.java
@@ -0,0 +1,13 @@
+package org.example.P3021;
+
+public class Solution {
+    public long flowerGame(int n, int m) {
+        long nn = n;
+        long mm = m;
+        long resultado = 0;
+        long divisao = 2;
+        resultado = (nn * mm) / divisao;
+
+        return resultado;
+    }
+}
